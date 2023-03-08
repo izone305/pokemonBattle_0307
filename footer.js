568dd206ee4cc6ae3ea562200200e29723e7d6f2
euni.ryu <euni.ryu97@gmail.com>
2023-03-08 15:00:42 +0900

    스킬명+키보드 방향키 병합

--- footer.js
+++ footer.js
@@ -148,8 +148,8 @@ footer.children[1].children[3].addEventListener('mouseover', function () {
   footer.children[1].children[0].style.border = "white";
 });
 
-
 // 방향키
+let modalWarp = footer.nextSibling.nextSibling
 let arr = [0, 0];
 
 function changeFunc() {
@@ -193,20 +193,14 @@ window.addEventListener('keydown', function (event) {
     }
     changeFunc();
   }
+
   // Enter를 누르면 모달창 띄우기
   else if (event.key === 'Enter') {
     if (arr[0] === 0 && arr[1] === 0) {
       footer.children[0].children[1].addEventListener('click', function () {
         console.log("click");
+        modalWarp.style.display = "block";
       })
     }
   }
-
-let modalWarp = footer.nextSibling.nextSibling
-}
-// 모달창 띄우기
-footer.children[0].children[1].addEventListener('click', function() {
-  console.log("click");
-  modalWarp.style.display = "block";
-
-});
+})
