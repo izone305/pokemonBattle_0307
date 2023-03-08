2bbf16eeea5b546ea763a6a66117fd93b0a262dc
Bigbowltakestime <Bigbowltakestime@gmail.com>
2023-03-08 10:24:48 +0900

    0308 아침 완료(개선 전)

---
+++ energyBar.js
@@ -0,0 +1,11 @@
+function hplose(hp){ 
+  hp.style.width="50%"; 
+  hp.style.background="red"; 
+} 
+  
+function hpfull(hpf){ 
+  hpf.style.width="100%"; 
+  hpf.style.height="35px";
+  hpf.style.display="flex"; 
+  hpf.style.background="green"; 
+}
