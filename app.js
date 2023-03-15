fb683c08e2791fa255bcf99a8e4567d9ece52880
euni.ryu <euni.ryu97@gmail.com>
2023-03-15 18:23:26 +0900

    일부 조금 수정

--- app.js
+++ app.js
@@ -21,81 +21,80 @@ const server = http.createServer(function(request, response){
   }
   if(request.method === 'GET' && request.url.startsWith('/battle')) {
     //배틀 화면 서버 창
-   if(request.url.split("/").length === 2){
-     fs.readFile("./index.html", function(err, data){
-       response.writeHead(200, {'Content-Type': 'text/html'});
-       response.write(data);
-       response.end();
-     })
-   }else if(request.url.split("/").length === 3){
-     
-     let name = request.url.split('/')[2];
-     console.log(name);
-     if(request.method === 'GET' && name === 'commonFunc.js'){
-     fs.readFile("./commonFunc.js", function(err, data){  
-       response.writeHead(200);
-       response.write(data);
-       response.end();
-     })
-     }
-     if(request.method === 'GET' && name === 'firstMain.js'){
-       fs.readFile("./firstMain.js", function(err, data){
-         response.writeHead(200);
-         response.write(data);
-         response.end();
-       })
-     }
-     if(request.method === 'GET' && name === 'energyChecker.js'){
-       fs.readFile("./energyChecker.js", function(err, data){
-         response.writeHead(200);
-         response.write(data);
-         response.end();
-       })
-     }
-     if(request.method === 'GET' && name === 'backAttack.js'){
-       fs.readFile("./backAttack.js", function(err, data){
-         response.writeHead(200);
-         response.write(data);
-         response.end();
-       })
-     }
-     if(request.method === 'GET' && name === 'modal.js'){
-       fs.readFile("./modal.js", function(err, data){
-         response.writeHead(200);
-         response.write(data);
-         response.end();
-       })
-     }
-     if(request.method === 'GET' && name === 'pokeSelect.js'){
-       fs.readFile("./pokeSelect.js", function(err, data){
-         response.writeHead(200);
-         response.write(data);
-         response.end();
-       })
-     }
-     if(request.method === 'GET' && name === 'footer.js'){
-       fs.readFile("./footer.js", function(err, data){
-         response.writeHead(200);
-         response.write(data);
-         response.end();
-       })
-     }
-     if(request.method === 'GET' && name === 'skillName.js'){
-       fs.readFile("./skillName.js", function(err, data){
-         response.writeHead(200);
-         response.write(data);
-         response.end();
-       })
-     }
-     if(request.method === 'GET' && name === 'runaway.js'){
-       fs.readFile("./runaway.js", function(err, data){
-         response.writeHead(200);
-         response.write(data);
-         response.end();
-       }
-       )
-     }
-     }
+    if(request.url.split("/").length === 2){
+      fs.readFile("./index.html", function(err, data){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+      })
+    }else if(request.url.split("/").length === 3){
+      
+      let name = request.url.split('/')[2];
+      console.log(name);
+      if(request.method === 'GET' && name === 'commonFunc.js'){
+      fs.readFile("./commonFunc.js", function(err, data){  
+        response.writeHead(200);
+        response.write(data);
+        response.end();
+      })
+      }
+      if(request.method === 'GET' && name === 'firstMain.js'){
+        fs.readFile("./firstMain.js", function(err, data){
+          response.writeHead(200);
+          response.write(data);
+          response.end();
+        })
+      }
+      if(request.method === 'GET' && name === 'energyChecker.js'){
+        fs.readFile("./energyChecker.js", function(err, data){
+          response.writeHead(200);
+          response.write(data);
+          response.end();
+        })
+      }
+      if(request.method === 'GET' && name === 'backAttack.js'){
+        fs.readFile("./backAttack.js", function(err, data){
+          response.writeHead(200);
+          response.write(data);
+          response.end();
+        })
+      }
+      if(request.method === 'GET' && name === 'modal.js'){
+        fs.readFile("./modal.js", function(err, data){
+          response.writeHead(200);
+          response.write(data);
+          response.end();
+        })
+      }
+      if(request.method === 'GET' && name === 'pokeSelect.js'){
+        fs.readFile("./pokeSelect.js", function(err, data){
+          response.writeHead(200);
+          response.write(data);
+          response.end();
+        })
+      }
+      if(request.method === 'GET' && name === 'footer.js'){
+        fs.readFile("./footer.js", function(err, data){
+          response.writeHead(200);
+          response.write(data);
+          response.end();
+        })
+      }
+      if(request.method === 'GET' && name === 'skillName.js'){
+        fs.readFile("./skillName.js", function(err, data){
+          response.writeHead(200);
+          response.write(data);
+          response.end();
+        })
+      }
+      if(request.method === 'GET' && name === 'runaway.js'){
+        fs.readFile("./runaway.js", function(err, data){
+          response.writeHead(200);
+          response.write(data);
+          response.end();
+        })
+      }
+    }
   }
   if(request.method === 'GET' && request.url.startsWith('/login')) {
 
