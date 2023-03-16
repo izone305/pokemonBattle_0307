5959a66a914fe7839c6d6f99993a104afd62a7e6
sumin <ujou173@gmail.com>
2023-03-16 10:23:53 +0900

    로그인창 병합 / html문서의 js 파일 경로 해결해야 할 듯

--- app.js
+++ app.js
@@ -2,23 +2,9 @@ const http = require('http');
 const fs = require("fs");
 //const pokemon = require('pokemon')
 
-// 1. 태그 만들기 o
-// 2. 스크립트 확인하기 o
-// 3. 잘되는지 확인하기 o
-// 4. 페이지 넘어가는지 확인하기 o
-//   4-1. url이 넘어가는지 확인 0
-// 5. url 값을 지정해주기 o
-//  5-1. url을 ? 앞 뒤로 잘라보기 o
-//  5-2. 자른 url을 한번 더 잘라보기 o
-// 6. url의 뒷부분을 숨길 수는 없나? -> 아직 잘 모르겠음
-// 7. 로그인 인증을 해보자 o
-//  7-1. id와 pw가 지정된 값일 경우 /battle로 이동 o
-//  7-2. 아닐경우 오류 메세지 출력  o
-// 8. 태그 만드는 부분 모듈로 따로 빼기 o
-
 // login 모듈, body 작성/form태그 작성
 const login = require('./login.js');
-let ID;
+// let ID;
 const server = http.createServer(function(request, response){
   // 최초접속
   if(request.method === 'GET' && request.url === '/') {
@@ -32,16 +18,18 @@ const server = http.createServer(function(request, response){
   
   if(request.method === 'GET' && request.url.startsWith('/battle')) {
     // history.replaceState({data: 'replace'}, '', '/battle'); //history.replaceState => url을 다른것으로 덮어씌워주는 녀석? node가 인식 못하는 듯
-    response.writeHead(200);
+    // response.writeHead(200);
     const urltest2 = request.url.split('?')[1];
     const idtest = urltest2.split('=')[1];
     const idtest2 = idtest.split('&')[0]; //id값
-    ID = idtest.split('&')[0];
-    const passtest = urltest2.split('=')[2]; //pw값
+    // ID = idtest.split('&')[0];
+    const passtest = urltest2.split('=')[2];
+    const passtest2 = passtest.split('/')[0]; //pw값
     console.log("idtest2 " + idtest2);
-    console.log("passtest " + passtest);
-    if(idtest2 === "KDT" && passtest === "305") { //입력값 비교해서 맞을 경우
+    console.log("passtest " + passtest2);
+    if(idtest2 === "KDT" && passtest2 === "305") { //입력값 비교해서 맞을 경우
       // response.write("success");
+      console.log(request.url.split("/").length);
       if(request.url.split("/").length === 2){
         fs.readFile("./index.html", function(err, data){
           response.writeHead(200, {'Content-Type': 'text/html'});
@@ -49,7 +37,6 @@ const server = http.createServer(function(request, response){
           response.end();
         })
       }else if(request.url.split("/").length === 3){
-        
         let name = request.url.split('/')[2];
         console.log(name);
         if(request.method === 'GET' && name === 'commonFunc.js'){
@@ -119,7 +106,7 @@ const server = http.createServer(function(request, response){
     } else {  //입력값 비교해서 틀릴 경우
       response.write("failed");
     }
-    response.end();
+    // response.end();
   }
   if(request.method === 'GET' && request.url.startsWith('/item')) {
     if(request.url.split('/').length === 2 ){
